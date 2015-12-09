d7ca41d2628119d46014b086d294f1ed66570b9a
rebuilder17 <rebuilder17@hotmail.com>
2015-12-09 21:43:06 +0900

    12.9.

--- Assets/FSNEngineExtensions/CameraControl/FSNCameraControl.cs
+++ Assets/FSNEngineExtensions/CameraControl/FSNCameraControl.cs
@@ -1,20 +1,129 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 /// <summary>
 /// 부착한 카메라에 대해 스크립트 레벨 컨트롤, 기기의 자이로스코프 연동 움직임
 /// </summary>
 public class FSNCameraControl : MonoBehaviour
 {
+	// Properties
+
+	[SerializeField]
+	bool            m_useGyroMovement = true;       // 자이로 센서에 따라 카메라가 살짝 이동
+	[SerializeField]
+	float           m_gyroMovementFactor = 0.5f;   // 자이로 센서에 따라 반응하여 움직이는 최대 거리
+	[SerializeField]
+	bool            m_useControlMovement = true;    // 외부 컨트롤을 통해 움직일 수 있는지
+
+
+	// Static Members
+
+	static List<FSNCameraControl>   s_instances = new List<FSNCameraControl>();
+	static void RegisterInstance(FSNCameraControl obj)
+	{
+		int count   = s_instances.Count;
+		for (int i = 0; i < count; i++)			// 중복 오브젝트 방지
+		{
+			if (s_instances[i] == obj)
+				return;
+		}
+		s_instances.Add(obj);					// 오브젝트 리스트에 추가
+	}
+
+	static void UnRegisterInstance(FSNCameraControl obj)
+	{
+		int count   = s_instances.Count;
+		for (int i = 0; i < count; i++)         // 오브젝트 제거
+		{
+			if (s_instances[i] == obj)
+			{
+				s_instances.RemoveAt(i);
+				return;
+			}
+		}
+	}
+	
+	/// <summary>
+	/// 카메라 위치 조정
+	/// </summary>
+	public static Vector3 controlPosition
+	{
+		set
+		{
+			int count   = s_instances.Count;
+			for(int i = 0; i < count; i++)
+			{
+				s_instances[i].m_controlPosition = value;
+            }
+		}
+	}
+
+
 	// Members
 
-	
+	Transform           m_tr;
+	Vector3             m_originalPosition;
+	protected Vector3	m_controlPosition;		// 컨트롤로 조종되는 좌표
+
+
+	void Awake()
+	{
+		if (m_useGyroMovement)
+			Input.gyro.enabled  = true;
+
+		m_tr				= transform;
+		m_originalPosition  = m_tr.localPosition;
+		RegisterInstance(this);
+	}
+
+	void OnDestroy()
+	{
+		UnRegisterInstance(this);
+	}
+
+
+	void Update()
+	{
+		Vector3 finalPosition   = m_originalPosition;
+
+		if (m_useControlMovement)					// 외부 컨트롤 좌표 추가
+		{
+			finalPosition		+= m_controlPosition;
+		}
+
+		if (m_useGyroMovement)						// 자이로 센서 계산 추가
+		{
+			var rotEuler        = RotateEulerAxisAroundZ(Input.gyro.attitude).eulerAngles;
+			Vector3 movement    = new Vector3();
+			movement.x          = Mathf.Sin(rotEuler.y / 180.0f * Mathf.PI) * m_gyroMovementFactor;
+			movement.y          = Mathf.Sin(-rotEuler.x / 180.0f * Mathf.PI) * m_gyroMovementFactor;
+			
+			finalPosition       += movement;
+		}
+
+		m_tr.localPosition      = finalPosition;
+		_testpos = finalPosition;
+    }
 
 	/// <summary>
-	/// 스크립트로 컨트롤하는 위치
+	/// Euler각에서 Z축 회전을 없애고 x,y 회전축을 Z축 회전각에 따라 회전시킨다.
 	/// </summary>
-	public Vector3 controlPosition
+	/// <param name="original"></param>
+	/// <returns></returns>
+	Quaternion RotateEulerAxisAroundZ(Quaternion original)
 	{
-		get; set;
+		var euler       = original.eulerAngles;
+		var zRotate     = Quaternion.AngleAxis(-euler.z, Vector3.forward);
+		var rotXAxis    = zRotate * Vector3.right;
+		var rotYAxis    = zRotate * Vector3.up;
+
+		return Quaternion.AngleAxis(euler.x, rotXAxis) * Quaternion.AngleAxis(euler.y, rotYAxis);
+	}
+
+	Vector3 _testpos = Vector3.zero;
+	void OnGUI()
+	{
+		GUI.Label(new Rect(10, 100, 300, 50), _testpos.ToString());
 	}
 }
--- Assets/GyroTest.cs
+++ Assets/GyroTest.cs
@@ -3,13 +3,45 @@ using System.Collections;
 
 public class GyroTest : MonoBehaviour
 {
+	Vector3 m_gyroEuler = new Vector3();
+
+
 	void Awake()
 	{
 		Input.gyro.enabled  = true;
 	}
 
+	void Start()
+	{
+		var testEuler   = new Vector3(0, 30, 90);
+		
+		Debug.Log(RotateEulerAnglesAroundZ(Quaternion.Euler(testEuler)).eulerAngles);
+	}
+
+	Quaternion RotateEulerAnglesAroundZ(Quaternion original)
+	{
+		var euler       = original.eulerAngles;
+		var zRotate     = Quaternion.AngleAxis(-euler.z, Vector3.forward);
+		var rotXAxis    = zRotate * Vector3.right;
+		var rotYAxis    = zRotate * Vector3.up;
+
+		return Quaternion.AngleAxis(euler.x, rotXAxis) * Quaternion.AngleAxis(euler.y, rotYAxis);
+	}
+
 	void Update()
 	{
-		transform.rotation = Quaternion.Euler(Input.gyro.attitude.eulerAngles * 0.3f);
+		var euler		= RotateEulerAnglesAroundZ(Input.gyro.attitude).eulerAngles;
+		m_gyroEuler.x   = euler.y;		// Landscape Left에 맞도록 좌표축 변경
+		m_gyroEuler.y   = -euler.x;
+		
+		var campos      = transform.position;
+		campos.x        = Mathf.Sin(m_gyroEuler.x / 180.0f * Mathf.PI) * 2;
+		campos.y        = Mathf.Sin(m_gyroEuler.y / 180.0f * Mathf.PI) * 2;
+		transform.position = campos;
+	}
+
+	void OnGUI()
+	{
+		GUI.Label(new Rect(10, 10, 400, 300), string.Format("rotation\nX : {0}\nY : {1}\nZ : {2}", m_gyroEuler.x, m_gyroEuler.y, m_gyroEuler.z));
 	}
 }
